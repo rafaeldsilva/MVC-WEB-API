0e234b2d6f0124f7b04a432d58def037714d422e
Rafael silva <rafael.dsilva@outlook.com.br>
2017-09-23 17:24:55 -0300

    Alteração da classe DbContext.

--- MCV-WEB-API/Models/MCV_WEB_APIContext.cs
+++ MCV-WEB-API/Models/MCV_WEB_APIContext.cs
@@ -4,21 +4,13 @@ namespace MCV_WEB_API.Models
 {
     public class MCV_WEB_APIContext : DbContext
     {
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
         public MCV_WEB_APIContext() : base("" +
-            "Data Source=(local);" +
-            "Initial Catalog=db_mwapi;" +
-            "Persist Security Info=True;" +
-            "User ID=sa;" +
-            "Password=sql2017"){
+            "Data Source=(local);" + //servidor hostname.
+            "Initial Catalog=db_mwapi;" + // Banco de dados.
+            "Persist Security Info=True;" + // armazenar senha na string de conexao.
+            "User ID=sa;" + //Usuario "sa".
+            "Password=sql2017"){ //Senha do banco.
         }
-
         public System.Data.Entity.DbSet<MCV_WEB_API.Models.Usuario> Usuarios { get; set; }
     }
 }
